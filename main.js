9fb2ff9cba55564184b3e783f158eb71bc3abcbb
LachlanMcL <zombieljm@yahoo.com>
2024-03-21 10:21:51 +1100

    added main.js logic and a simple index.html for future use

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+
+const isMac = process.platform === 'darwin'
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: "Image Resizer",
+        width: 500,
+        height: 500,
+    })
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+    })
+})
+
+//depending on system, app will close differently.
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit()
+    }
+})
